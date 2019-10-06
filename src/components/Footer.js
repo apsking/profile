9e5cc040a0bd21afbaed14fe9d814a34b2a16c9c
Alex King <alkin@amazon.com>
2019-10-06 12:05:34 -0500

    Add CNAME, Update readability of content. Add Puppy Cant Hang.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">Design by <a href="https://html5up.net">HTML5 UP</a></p>
+        <p className="copyright">Built with <a href="https://www.gatsbyjs.org/">GatsbyJS</a> and <a href="https://html5up.net">HTML5 UP</a></p>
     </footer>
 )
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -57,16 +57,36 @@ class Main extends React.Component {
         >
           <h2 className="major">Projects</h2>
           <h3>
+            Instagram - Puppy Can't Hang
+          </h3>
+          <p>
+            For a long time, I wanted to create an Instagram following. I decided
+            to create <a href="https://www.instagram.com/puppy.cant.hang/">Puppy Can't Hang</a> after
+            finding the niche that I just couldn't pass up - Sleeping puppies! Puppy
+            Can't Hang is all about sharing those precious moments of our four-legged
+            friends who just want to snooze!
+          </p>
+          <h3>
 			Alexa Skill - My Pet Rock
 		  </h3>
 		  <p>
-			In <a href="https://www.amazon.com/Alexander-King-My-Pet-Rock/dp/B073VDPTCW" target="_blank" rel="noopener noreferrer">My Pet Rock</a>, you get to take care of your own pet. Take your new pet on walks, play games with it, and feed it regularly, so that it grows big and strong! Your pet will grow up over time to learn more skills and tricks. Join nearly than 500,000 other people and see what it's all about!
+			In <a href="https://www.amazon.com/Alexander-King-My-Pet-Rock/dp/B073VDPTCW" target="_blank" rel="noopener noreferrer">My Pet Rock</a>,
+            you get to take care of your own pet. Take your new pet on walks,
+            play games with it, and feed it regularly, so that it grows big and
+            strong! Your pet will grow up over time to learn more skills and tricks.
+            Join nearly than 500,000 other people and see what it's all about!
 		  </p>
           <h3>
 			Alexa Skill - Interview Prep
 		  </h3>
 		  <p>
-			<a href="https://www.amazon.com/Alexander-King-Interview-Prep/dp/B01LW7L2VD" target="_blank"rel="noopener noreferrer">Interview Prep</a> is your one-stop skill to help you prepare for your next interview! It has hundreds of questions for a variety of different job types and areas of work. For best results, ask Interview Prep a few questions every day to prepare your answers. Consider answering directly into a mirror and focusing not only on your answer, but also your body language!
+			<a href="https://www.amazon.com/Alexander-King-Interview-Prep/dp/B01LW7L2VD" target="_blank"rel="noopener noreferrer">Interview Prep</a> is
+            your one-stop skill to help you prepare for your next interview!
+            It has hundreds of questions for a variety of different job types
+            and areas of work. For best results, ask Interview Prep a few
+            questions every day to prepare your answers. Consider answering
+            directly into a mirror and focusing not only on your answer, but
+            also your body language!
 		  </p>
           <p>
             This skill was recently featured on <a href="https://lifehacker.com/how-to-use-alexa-to-help-you-prep-for-a-job-interview-1833024806">LifeHacker!</a>
@@ -75,7 +95,12 @@ class Main extends React.Component {
 			Alexa Skill - Ancient Alchemy
 		  </h3>
 		  <p>
-			In <a href="https://www.amazon.com/Alexander-King-Ancient-Alchemy/dp/B074F4NHJ8" target="_blank"rel="noopener noreferrer">Ancient Alchemy</a>, you take on the challenge of restoring the Library of Elements by finding recipes for all of the elements in the realm. As The Alchemist, you start by combining primitive elements and then continue to use your findings to make many more! This creative journey will immerse you in its world and challenge your abilities!
+			In <a href="https://www.amazon.com/Alexander-King-Ancient-Alchemy/dp/B074F4NHJ8" target="_blank"rel="noopener noreferrer">Ancient Alchemy</a>, you
+            take on the challenge of restoring the Library of Elements by finding
+            recipes for all of the elements in the realm. As The Alchemist, you
+            start by combining primitive elements and then continue to use your
+            findings to make many more! This creative journey will immerse you
+            in its world and challenge your abilities!
 		  </p>
           {close}
         </article>
